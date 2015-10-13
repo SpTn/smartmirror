290adee6ca2ff438442a42e8505ab6a29f5db21c
Lotfi Messaoudi <volkovmqx@gmail.com>
2015-10-13 19:53:32 +0200

    Initial Release

---
+++ js/config.js
@@ -0,0 +1,37 @@
+// for navigator language
+// var lang = window.navigator.language;
+// you can change the language
+var lang = 'en';
+
+//change weather params here:
+//units: metric or imperial
+var weatherParams = {
+    'q':'Sussah,Tunisie',
+    'units':'metric',
+    'lang':lang,
+    'APPID':'2e4f83fb1a4a7db72f8bc8ed6077aeb3'
+};
+
+var feed = 'https://queryfeed.net/tw?q=%23%E2%80%8ESFD2015TN%E2%80%AC';
+//var feed = 'http://www.nu.nl/feeds/rss/achterklap.rss';
+//var feed = 'http://www.nu.nl/feeds/rss/opmerkelijk.rss';
+//var feed = 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml';
+
+// compliments:
+var morning = [
+            'Good morning, handsome!',
+            'Enjoy your day!',
+            'How was your sleep?'
+        ];
+
+var afternoon = [
+            'Hello, beauty!',
+            'You look sexy!',
+            'Looking good today!'
+        ];
+
+var evening = [
+            'Wow, you look hot!',
+            'You look nice!',
+            'Hi, sexy!'
+        ];
